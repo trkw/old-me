1eef9b2f09dc8a5e25e8d46b1e16733aac7e564e
trkw <trkw@trkw.me>
2016-11-03 22:59:42 +0900

    Update profileimage

--- src/components/ProfileImage/index.js
+++ src/components/ProfileImage/index.js
@@ -2,12 +2,7 @@ import React from "react"
 import styles from "./index.css"
 
 const ProfileImage = () => (
-  <div className={ styles.imagewrap }>
-    <img
-      src={ "https://avatars1.githubusercontent.com/u/2557813" }
-      alt={ "Naohiro Terakawa" }
-    />
-  </div>
+  <div className={ styles.imagewrap } />
 )
 
 export default ProfileImage
